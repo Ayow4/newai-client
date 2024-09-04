8024c14983581d2c18b50a5300c2867008064305
Ayow4 <chilko456@gmail.com>
2024-09-04 14:40:33 +0800

    env update

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json())
 
 const connect = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/aichat');
+    await mongoose.connect(process.env.MONGO);
     console.log('Connected to MongoDB!');
   } catch (err) {
     console.error("Failed to connect to MongoDB: ", err);
